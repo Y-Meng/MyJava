7df0cbadba1a1d8dd6792bbd4c133cd0b7f3f262
marin <545127804@qq.com>
2018-04-25 14:10:31 +0800

    http工具类修改

--- plat-base/ssm-base/src/main/java/com/mingtai/base/util/http/HttpRequest.java
+++ plat-base/ssm-base/src/main/java/com/mingtai/base/util/http/HttpRequest.java
@@ -4,10 +4,7 @@ import com.mingtai.base.util.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
+import java.io.*;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.HashMap;
@@ -116,12 +113,12 @@ public class HttpRequest {
             // Post 请求不能使用缓存
             conn.setUseCaches(false);
 
-            // 写入post数据
-            PrintWriter writer = new PrintWriter(conn.getOutputStream());
+            // 写入post数据；注意编码：utf-8
+            OutputStreamWriter writer = new OutputStreamWriter (conn.getOutputStream(),"utf-8");
             if(StringUtils.isBlank(body)) {
-                writer.print(getParamsString());
+                writer.write(getParamsString());
             }else{
-                writer.print(body);
+                writer.write(body);
             }
             writer.flush();
             writer.close();
