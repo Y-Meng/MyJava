5e7c18d9426e3a9feb0be8ab90ae8d1cc92464f3
marin <545127804@qq.com>
2018-08-08 11:54:07 +0800

    gzip工具

---
+++ plat-base/ssm-base/src/main/java/com/mingtai/base/util/GzipUtil.java
@@ -0,0 +1,64 @@
+package com.mingtai.base.util;
+
+import org.springframework.util.Assert;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.zip.GZIPInputStream;
+import java.util.zip.GZIPOutputStream;
+
+/**
+ * @author zkzc-mcy create at 2018/8/8.
+ */
+public class GzipUtil {
+
+    /**
+     * gzip压缩
+     * @param content
+     * @param charset
+     * @return
+     * @throws IOException
+     */
+    public static byte[] compress(String content, String charset) throws IOException {
+
+        Assert.notNull(content, "null compress error");
+        try (ByteArrayOutputStream out = new ByteArrayOutputStream();
+             GZIPOutputStream gzip = new GZIPOutputStream(out)){
+            gzip.write(content.getBytes(charset));
+            gzip.close();
+            return out.toByteArray();
+        }
+    }
+
+    /**
+     * gizp解压
+     * @param in
+     * @return
+     * @throws IOException
+     */
+    public static byte[] uncompress(InputStream in) throws IOException {
+
+        try(ByteArrayOutputStream out = new ByteArrayOutputStream();
+            GZIPInputStream gin = new GZIPInputStream(in)){
+            byte[] buffer = new byte[1024];
+            int len;
+            while ((len = gin.read(buffer)) >= 0){
+                out.write(buffer, 0 ,len);
+            }
+            return out.toByteArray();
+        }
+    }
+
+    /**
+     * 解压缩为字符串
+     * @param in
+     * @param charset
+     * @return
+     * @throws IOException
+     */
+    public static String uncompress(InputStream in, String charset) throws IOException {
+
+        byte[] bytes = uncompress(in);
+        return new String(bytes, charset);
+    }
+}
